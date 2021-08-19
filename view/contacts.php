54212cbe31ebaadd849d515d313e938c56770533
bakerko <ua.nikutin@gmail.com>
2021-08-19 14:22:20 +0300

    view contacts goggle map point

--- view/contacts.php
+++ view/contacts.php
@@ -27,6 +27,11 @@
             <span class="delivery"><i style="font-size: 30pt;" class="fas fa-phone table_icon" src=""></i><b>Контакты</b></span>
         </li>
 
+        <li>
+            <div style="margin-left: calc(50% - 300px);" >
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10581.384429834186!2d35.0474254!3d48.4690745!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xaa1332b77b8ee413!2zQk9PQk9OINCz0LXQu9C40LXQstGL0LUg0YjQsNGA0Ysg0LTQvdC10L_RgA!5e0!3m2!1sru!2sua!4v1629371572192!5m2!1sru!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </li>
         <li class="noMargin_flexbox bordered"><p>Телефон</p></li>
         <li class="noMargin_flexbox bordered"><p>Мейл</p></li>
         <li class="noMargin_flexbox bordered"><p>Инстаграм</p></li>
