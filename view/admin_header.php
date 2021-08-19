31ce556492f38af460c991d9ebe1ccb4897cd559
bakerko <ua.nikutin@gmail.com>
2021-08-19 13:06:36 +0300

    adminka search block

--- view/admin_header.php
+++ view/admin_header.php
@@ -4,8 +4,19 @@
 
     <div class="search_block">
         <div class="container flexbox flex_mob">
-            <div><a href="/home/"><img class="logo" src="/images/logo.jpg"></a></div>
-
+            <div><a href="/home/"><img class="logo" src="/images/logo.jpg"/></a></div>
+            <div class="search_field">
+                <form id="search" method="post" action="/home/search">
+                    <input class="form-control" type="text" aria-label="Search" name="search_text">
+                    <img onclick="document.getElementById('search').submit();" class="search_img" src="/images/search.png"/>
+                </form>
+            </div>
+            <a href="/home/cart">
+                <div class="cart_box">
+                    <img class="cart" src="/images/Cart.png" height="450" width="450"/>
+                    <p><b>: <b id="total_price_head"><?php if(isset($_SESSION['total_price'])){echo $_SESSION['total_price'];}else{echo 0;}?></b>&nbsp; грн</b></p>
+                </div>
+            </a>
         </div>
     </div>
 </header>
